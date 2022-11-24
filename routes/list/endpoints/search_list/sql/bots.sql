011f2d65efa75d8dd0989a58b8793bbee1c0f1cd
Your Name <you@example.com>
2022-11-24 18:24:52 +0000

    f

--- routes/list/endpoints/search_list/sql/bots.sql
+++ routes/list/endpoints/search_list/sql/bots.sql
@@ -7,7 +7,7 @@ AND (($X+1 = -1) OR (servers <= $X+1))
 */
 
 SELECT DISTINCT {cols} FROM bots
-WHERE type = 'approved' OR type = 'certified'
+WHERE (type = 'approved' OR type = 'certified')
 AND (queue_name ILIKE $2 OR vanity ILIKE $2 OR owner @@ $1 OR short @@ $1) 
 
 -- Guild count filter (3-4)
@@ -25,4 +25,4 @@ AND (($8 = -1) OR (shards <= $8))
 -- Tags filter
 AND (cardinality($9::text[]) = 0 OR tags {op} $9) -- Where op is one of @> = all, && = any
 
-ORDER BY votes DESC, certified DESC LIMIT 6
+ORDER BY votes DESC, type DESC LIMIT 6
