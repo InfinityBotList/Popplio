0ec6cf57c0ee453ff2086d3441a5a435e1a00d0a
Your Name <you@example.com>
2023-01-14 13:36:25 +0000

    add add pack api

--- routes/packs/endpoints/add_pack/route.go
+++ routes/packs/endpoints/add_pack/route.go
@@ -1,1 +1,125 @@
 package add_pack
+
+import (
+	"net/http"
+	"popplio/api"
+	"popplio/docs"
+	"popplio/state"
+	"popplio/types"
+	"popplio/utils"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var compiledMessages = api.CompileValidationErrors(CreatePack{})
+
+type CreatePack struct {
+	Name  string   `json:"name" validate:"required,min=3,max=20" msg:"Name must be between 3 and 20 characters"`
+	URL   string   `json:"url" validate:"required,min=3,max=20,nospaces,notblank,alpha" msg:"URL must be between 3 and 20 characters without spaces and must be alphabetic"`
+	Short string   `json:"short" validate:"required,min=30,max=100" msg:"Description must be between 30 and 100 characters"`
+	Tags  []string `json:"tags" validate:"required,unique,min=1,max=5,dive,min=3,max=20,alpha,notblank,nonvulgar,nospaces" msg:"There must be between 1 and 5 tags without duplicates" amsg:"Each tag must be between 3 and 20 characters and alphabetic"`
+	Bots  []string `json:"bots" validate:"required,unique,min=1,max=10,dive,numeric" msg:"There must be between 1 and 10 bots without duplicates"`
+}
+
+func Docs() *docs.Doc {
+	return &docs.Doc{
+		Method:      "PUT",
+		Path:        "/users/{user_id}/packs",
+		Summary:     "Create Pack",
+		Description: "Creates a pack. Returns 204 on success",
+		Resp:        types.AllPacks{},
+	}
+}
+
+func Route(d api.RouteData, r *http.Request) api.HttpResponse {
+	var payload CreatePack
+
+	hresp, ok := api.MarshalReq(r, &payload)
+
+	if !ok {
+		return hresp
+	}
+
+	// Validate the payload
+	err := state.Validator.Struct(payload)
+
+	if err != nil {
+		errors := err.(validator.ValidationErrors)
+		return api.ValidatorErrorResponse(compiledMessages, errors)
+	}
+
+	// Check that all bots exist
+	for _, bot := range payload.Bots {
+		botUser, err := utils.GetDiscordUser(bot)
+
+		if err != nil {
+			return api.HttpResponse{
+				Status: http.StatusBadRequest,
+				Json: types.ApiError{
+					Message: "One of the bot you wish to add does not exist [" + bot + "]: " + err.Error(),
+					Error:   true,
+				},
+			}
+		}
+
+		if !botUser.Bot {
+			return api.HttpResponse{
+				Status: http.StatusBadRequest,
+				Json: types.ApiError{
+					Message: "One of the bot you wish to add is not actually a bot [" + bot + "]",
+					Error:   true,
+				},
+			}
+		}
+	}
+
+	// Check that the pack does not already exist
+	var count int64
+	err = state.Pool.QueryRow(d.Context, "SELECT * FROM packs WHERE url = $1", payload.URL).Scan(&count)
+
+	if err != nil {
+		return api.HttpResponse{
+			Status: http.StatusBadRequest,
+			Json: types.ApiError{
+				Message: err.Error(),
+				Error:   true,
+			},
+		}
+	}
+
+	if count > 0 {
+		return api.HttpResponse{
+			Status: http.StatusBadRequest,
+			Json: types.ApiError{
+				Message: "A pack with that URL already exists",
+				Error:   true,
+			},
+		}
+	}
+
+	// Create the pack
+	_, err = state.Pool.Exec(
+		d.Context,
+		"INSERT INTO packs (name, url, short, tags, bots, owner) VALUES ($1, $2, $3, $4, $5, $6)",
+		payload.Name,
+		payload.URL,
+		payload.Short,
+		payload.Tags,
+		payload.Bots,
+		d.Auth.ID,
+	)
+
+	if err != nil {
+		return api.HttpResponse{
+			Status: http.StatusBadRequest,
+			Json: types.ApiError{
+				Message: err.Error(),
+				Error:   true,
+			},
+		}
+	}
+
+	return api.HttpResponse{
+		Status: http.StatusNoContent,
+	}
+}
