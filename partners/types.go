bede5039d8843f700efd21dd9b390ca3a9cb337c
Your Name <you@example.com>
2023-02-10 15:38:36 +0000

    ...

---
+++ partners/types.go
@@ -0,0 +1,21 @@
+package partners
+
+import "popplio/types"
+
+type Partner struct {
+	ID     string       `json:"id" validate:"required"`
+	Name   string       `json:"name" validate:"required"`
+	Image  string       `json:"image" validate:"required"`
+	Short  string       `json:"short" validate:"required"`
+	Links  []types.Link `json:"links" validate:"required,min=1,max=2"`
+	UserID string       `json:"-" validate:"required,numeric"`
+
+	// Internal field
+	User *types.DiscordUser `json:"user" validate:"notpresent"`
+}
+
+type PartnerList struct {
+	Featured        []Partner `json:"featured" validate:"required"`
+	BotPartners     []Partner `json:"bot_partners" validate:"required"`
+	BotListPartners []Partner `json:"bot_list_partners" validate:"required"`
+}
