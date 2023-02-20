57d613e1742dfcd01569812426a52e842101a069
Your Name <you@example.com>
2023-02-20 11:17:35 +0000

    d

--- routes/blogs/endpoints/get_blog_list/route.go
+++ routes/blogs/endpoints/get_blog_list/route.go
@@ -43,6 +43,15 @@ func Route(d api.RouteData, r *http.Request) api.HttpResponse {
 		return api.DefaultResponse(http.StatusInternalServerError)
 	}
 
+	for i := range blogPosts {
+		blogPosts[i].Author, err = utils.GetDiscordUser(d.Context, blogPosts[i].UserID)
+
+		if err != nil {
+			state.Logger.Error(err)
+			return api.DefaultResponse(http.StatusInternalServerError)
+		}
+	}
+
 	return api.HttpResponse{
 		Json: types.Blog{
 			Posts: blogPosts,
