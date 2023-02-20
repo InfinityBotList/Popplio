7b9819ffc862ec5e435398718507d241dafb880d
Your Name <you@example.com>
2023-02-20 11:38:17 +0000

    f

---
+++ routes/blogs/endpoints/delete_blog_post/route.go
@@ -0,0 +1,68 @@
+package delete_blog_post
+
+import (
+	"net/http"
+	"popplio/api"
+	"popplio/docs"
+	"popplio/state"
+	"popplio/types"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Docs() *docs.Doc {
+	return &docs.Doc{
+		Summary:     "Delete Blog List",
+		Description: "Deletes a blog post on the list. You must be an `iblhdev` or an `hadmin` to delete a blog post.",
+		Params: []docs.Parameter{
+			{
+				Name:        "user_id",
+				Description: "The ID of the user who is deleting the blog post.",
+				Required:    true,
+				In:          "path",
+				Schema:      docs.IdSchema,
+			},
+			{
+				Name:        "slug",
+				Description: "The slug of the blog post to delete.",
+				Required:    true,
+				In:          "path",
+				Schema:      docs.IdSchema,
+			},
+		},
+		Resp: types.ApiError{},
+	}
+}
+
+func Route(d api.RouteData, r *http.Request) api.HttpResponse {
+	// Check if user is iblhdev or hadmin
+	var iblhdev bool
+	var hadmin bool
+
+	err := state.Pool.QueryRow(d.Context, "SELECT iblhdev, hadmin FROM users WHERE user_id = $1", d.Auth.ID).Scan(&iblhdev, &hadmin)
+
+	if err != nil {
+		state.Logger.Error(err)
+		return api.DefaultResponse(http.StatusInternalServerError)
+	}
+
+	if !iblhdev && !hadmin {
+		return api.HttpResponse{
+			Status: http.StatusForbidden,
+			Json: types.ApiError{
+				Message: "You do not have permission to create a blog post",
+				Error:   true,
+			},
+		}
+	}
+
+	// Delete the blog post
+	_, err = state.Pool.Exec(d.Context, "DELETE FROM blog_posts WHERE slug = $1", chi.URLParam(r, "slug"))
+
+	if err != nil {
+		state.Logger.Error(err)
+		return api.DefaultResponse(http.StatusInternalServerError)
+	}
+
+	return api.DefaultResponse(http.StatusNoContent)
+}
--- routes/blogs/router.go
+++ routes/blogs/router.go
@@ -3,6 +3,7 @@ package blogs
 import (
 	"popplio/api"
 	"popplio/routes/blogs/endpoints/create_blog_post"
+	"popplio/routes/blogs/endpoints/delete_blog_post"
 	"popplio/routes/blogs/endpoints/get_blog_list"
 	"popplio/types"
 
@@ -24,6 +25,20 @@ func (b Router) Routes(r *chi.Mux) {
 		Method:  api.POST,
 		Docs:    create_blog_post.Docs,
 		Handler: create_blog_post.Route,
+		Auth: []api.AuthType{
+			{
+				URLVar: "user_id",
+				Type:   types.TargetTypeUser,
+			},
+		},
+	}.Route(r)
+
+	api.Route{
+		Pattern: "/users/{user_id}/blog",
+		OpId:    "delete_blog_post",
+		Method:  api.DELETE,
+		Docs:    delete_blog_post.Docs,
+		Handler: delete_blog_post.Route,
 		Auth: []api.AuthType{
 			{
 				URLVar: "user_id",
