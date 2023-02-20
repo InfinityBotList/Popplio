7d54c99be4f65335a7c39d5c2cc498db5f86f844
Your Name <you@example.com>
2023-02-20 18:55:39 +0000

    r

--- routes/list/endpoints/current_status/route.go
+++ routes/list/endpoints/current_status/route.go
@@ -8,70 +8,33 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-type ListStatus struct {
-	StatusPage StatusSummary `json:"status_page"` // From https://status.botlist.site/summary.json
-	ApiStatus  ApiStatus     `json:"api_status"`
-}
-
-type StatusSummary struct {
-	Name   string `json:"name"`
-	Url    string `json:"url"`
-	Status string `json:"status"`
-}
-
-type ApiStatus struct {
-	ApiUp bool `json:"api_up"`
-}
-
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func Docs() *docs.Doc {
 	return &docs.Doc{
 		Summary:     "Get Current Status",
 		Description: "Gets the current status of the list",
-		Resp:        ListStatus{},
+		Resp:        map[string]any{},
 	}
 }
 
 func Route(d api.RouteData, r *http.Request) api.HttpResponse {
-	var listStatus ListStatus
+	var listStatus map[string]any
 
 	res, err := http.Get("https://status.botlist.site/summary.json")
 
 	if err != nil {
-		listStatus.StatusPage = StatusSummary{
-			Name:   "Infinity Bots",
-			Url:    "https://status.botlist.site",
-			Status: "STATUSPAGEDOWN",
-		}
+		return api.DefaultResponse(http.StatusInternalServerError)
 	}
 
 	if res.StatusCode != 200 {
-		listStatus.StatusPage = StatusSummary{
-			Name:   "Infinity Bots",
-			Url:    "https://status.botlist.site",
-			Status: "STATUSPAGEDOWN",
-		}
+		return api.DefaultResponse(http.StatusInternalServerError)
 	}
 
-	var sp struct {
-		Page StatusSummary `json:"page"`
-	}
-
-	err = json.NewDecoder(res.Body).Decode(&sp)
+	err = json.NewDecoder(res.Body).Decode(&listStatus)
 
 	if err != nil {
-		listStatus.StatusPage = StatusSummary{
-			Name:   "Infinity Bots",
-			Url:    "https://status.botlist.site",
-			Status: "STATUSPAGEBADRESP",
-		}
-	}
-
-	listStatus.StatusPage = sp.Page
-
-	listStatus.ApiStatus = ApiStatus{
-		ApiUp: true,
+		return api.DefaultResponse(http.StatusInternalServerError)
 	}
 
 	return api.HttpResponse{
