035883d350127c245ba6bfe9ab4799da84c0df9e
Your Name <you@example.com>
2023-02-25 16:21:22 +0000

    add

--- routes/teams/endpoints/get_team_permissions/route.go
+++ routes/teams/endpoints/get_team_permissions/route.go
@@ -4,7 +4,11 @@ import (
 	"net/http"
 	"popplio/api"
 	"popplio/docs"
+	"popplio/state"
 	"popplio/teams"
+	"popplio/utils"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type PermissionResponse struct {
@@ -29,6 +33,26 @@ func Docs() *docs.Doc {
 }
 
 func Route(d api.RouteData, r *http.Request) api.HttpResponse {
+	id := chi.URLParam(r, "id")
+
+	// Convert ID to UUID
+	if !utils.IsValidUUID(id) {
+		return api.DefaultResponse(http.StatusNotFound)
+	}
+
+	var count int
+
+	err := state.Pool.QueryRow(d.Context, "SELECT COUNT(*) FROM teams WHERE id = $1", id).Scan(&count)
+
+	if err != nil {
+		state.Logger.Error(err)
+		return api.DefaultResponse(http.StatusInternalServerError)
+	}
+
+	if count == 0 {
+		return api.DefaultResponse(http.StatusNotFound)
+	}
+
 	return api.HttpResponse{
 		Json: PermissionResponse{teams.TeamPermDetails},
 	}
