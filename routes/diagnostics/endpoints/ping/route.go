39136b09a17a8dd26dd87bc581d14fdb68b8869f
Your Name <you@example.com>
2023-03-12 06:26:17 +0000

    remove apiboturl

--- routes/diagnostics/endpoints/ping/route.go
+++ routes/diagnostics/endpoints/ping/route.go
@@ -14,17 +14,15 @@ import (
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type Hello struct {
-	Message   string `json:"message"`
-	Docs      string `json:"docs"`
-	OurSite   string `json:"our_site"`
-	Status    string `json:"status"`
-	APIBotURL string `json:"api_bot_url"`
+	Message string `json:"message"`
+	Docs    string `json:"docs"`
+	OurSite string `json:"our_site"`
+	Status  string `json:"status"`
 }
 
 const (
 	mainSite   = "https://infinitybotlist.com"
 	statusPage = "https://status.botlist.site"
-	apiBot     = "https://discord.com/api/oauth2/authorize?client_id=818419115068751892&permissions=140898593856&scope=bot%20applications.commands"
 )
 
 var helloWorld []byte
@@ -35,11 +33,10 @@ func Setup() {
 
 	// This is done here to avoid constant remarshalling
 	helloWorldB = Hello{
-		Message:   "Hello world from IBL API v6!",
-		Docs:      docsSite,
-		OurSite:   mainSite,
-		Status:    statusPage,
-		APIBotURL: apiBot,
+		Message: "Hello world from IBL API v6!",
+		Docs:    docsSite,
+		OurSite: mainSite,
+		Status:  statusPage,
 	}
 
 	var err error
