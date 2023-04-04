ea4796e36056b34583e45bae4f4faf33a2e121cd
Your Name <you@example.com>
2023-04-04 18:05:49 +0000

    fix

--- routes/duser/endpoints/clear_duser/route.go
+++ routes/duser/endpoints/clear_duser/route.go
@@ -2,8 +2,10 @@ package clear_duser
 
 import (
 	"net/http"
+	"time"
 
 	"popplio/api"
+	"popplio/ratelimit"
 	"popplio/state"
 	"popplio/types"
 
@@ -32,6 +34,28 @@ func Docs() *docs.Doc {
 func Route(d api.RouteData, r *http.Request) api.HttpResponse {
 	id := chi.URLParam(r, "id")
 	state.Redis.Del(d.Context, "uobj:"+id)
+
+	limit, err := ratelimit.Ratelimit{
+		Expiry:      1 * time.Minute,
+		MaxRequests: 3,
+		Bucket:      "clear_duser",
+	}.Limit(d.Context, r)
+
+	if err != nil {
+		state.Logger.Error(err)
+		return api.DefaultResponse(http.StatusInternalServerError)
+	}
+
+	if limit.Exceeded {
+		return api.HttpResponse{
+			Json: types.ApiError{
+				Error:   true,
+				Message: "You are being ratelimited. Please try again in " + limit.TimeToReset.String(),
+			},
+			Headers: limit.Headers(),
+			Status:  http.StatusTooManyRequests,
+		}
+	}
 
 	// Delete from internal_user_cache
 	state.Pool.Exec(d.Context, "DELETE FROM internal_user_cache WHERE id = $1", id)
