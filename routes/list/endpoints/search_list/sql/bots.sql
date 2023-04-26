57aa57c8fc9004aa7f838779ae4da01eb7b72f67
TheRealToxicDev <toxic.dev09@gmail.com>
2023-04-26 19:15:48 +0000

    improve

--- routes/list/endpoints/search_list/sql/bots.sql
+++ routes/list/endpoints/search_list/sql/bots.sql
@@ -6,7 +6,10 @@ AND (servers >= $X)
 AND (($X+1 = 0) OR (servers <= $X+1))
 */
 
-SELECT DISTINCT {{.Cols}} FROM bots
+SELECT DISTINCT {{.Cols}} FROM bots 
+{{if .Query}}
+    INNER JOIN internal_user_cache bot_users ON bots.bot_id = bot_users.id
+{{end}}
 WHERE (type = 'approved' OR type = 'certified')
 
 -- Guild count filter (1-2)
@@ -25,7 +28,7 @@ AND (($6 = 0) OR (shards <= $6))
 AND (cardinality($7::text[]) = 0 OR tags {{.TagMode}} $7) -- Where TagMode is one of @> = all, && = any
 
 {{if .Query}}
-AND (queue_name ILIKE $8 OR vanity ILIKE $8 OR owner @@ $9 OR short @@ $9 OR bot_id = $9 OR client_id = $9) 
+AND (bot_users.username @@ $8 OR vanity ILIKE $8 OR short @@ $9 OR bot_id = $9 OR client_id = $9) 
 {{end}}
 
 ORDER BY votes DESC, type DESC LIMIT 12
