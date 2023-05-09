421e31844f50129ee400e895a90e9053d24145d7
TheRealToxicDev <toxic.dev09@gmail.com>
2023-05-09 12:30:38 +0000

    fix

--- types/user.go
+++ types/user.go
@@ -5,21 +5,29 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
+type UserExperiment string
+
+const (
+	ServerListingUserExperiment UserExperiment = "SERVER_LISTING"
+)
+
 type UserLogin struct {
 	Token  string `json:"token" description:"The users token"`
 	UserID string `json:"user_id" description:"The users ID"`
 }
 
+// @ci table=users
 type User struct {
-	ITag                      pgtype.UUID           `db:"itag" json:"itag"`
+	ITag                      pgtype.UUID           `db:"itag" json:"itag" description:"The user's internal ID. An artifact of database migrations."`
 	ID                        string                `db:"user_id" json:"-"`
-	User                      *dovewing.DiscordUser `db:"-" json:"user"` // Must be handled internally
-	Experiments               []string              `db:"experiments" json:"experiments"`
+	User                      *dovewing.DiscordUser `db:"-" json:"user" ci:"internal"` // Must be handled internally
+	Experiments               []string              `db:"experiments" json:"experiments" description:"The experiments the user is in"`
 	StaffOnboarded            bool                  `db:"staff_onboarded" json:"staff_onboarded"`
 	StaffOnboardState         string                `db:"staff_onboard_state" json:"staff_onboard_state"`
 	StaffOnboardLastStartTime pgtype.Timestamptz    `db:"staff_onboard_last_start_time" json:"staff_onboard_last_start_time"`
 	StaffOnboardGuild         pgtype.Text           `db:"staff_onboard_guild" json:"staff_onboard_guild"`
 	StaffRPCLastVerify        pgtype.Timestamptz    `db:"staff_rpc_last_verify" json:"staff_rpc_last_verify"`
+	StaffOnboardSessionCode   string                `db:"staff_onboard_session_code" json:"staff_onboard_session_code" description:"The session code for the staff onboarding"`
 	Staff                     bool                  `db:"staff" json:"staff"`
 	Admin                     bool                  `db:"admin" json:"admin"`
 	HAdmin                    bool                  `db:"hadmin" json:"hadmin"`
@@ -30,13 +38,13 @@ type User struct {
 	BotDeveloper              bool                  `db:"developer" json:"bot_developer"`
 	BugHunters                bool                  `db:"bug_hunters" json:"bug_hunters"`
 	CaptchaSponsorEnabled     bool                  `db:"captcha_sponsor_enabled" json:"captcha_sponsor_enabled"`
-	ExtraLinks                []Link                `db:"extra_links" json:"extra_links"`
+	ExtraLinks                []Link                `db:"extra_links" json:"extra_links" description:"The users links that it wishes to advertise"`
 	About                     pgtype.Text           `db:"about" json:"about"`
 	VoteBanned                bool                  `db:"vote_banned" json:"vote_banned"`
 	Banned                    bool                  `db:"banned" json:"banned"`
-	UserBots                  []UserBot             `json:"user_bots"`  // Must be handled internally
-	UserTeams                 []Team                `json:"user_teams"` // Must be handled internally
-	UserPacks                 []IndexBotPack        `json:"user_packs"` // Must be handled internally
+	UserBots                  []UserBot             `json:"user_bots" ci:"internal"`  // Must be handled internally
+	UserTeams                 []Team                `json:"user_teams" ci:"internal"` // Must be handled internally
+	UserPacks                 []IndexBotPack        `json:"user_packs" ci:"internal"` // Must be handled internally
 }
 
 type UserBot struct {
