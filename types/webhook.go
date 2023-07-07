a88145226d15eea6ec8e551d95a0989ffe42811a
Your Name <you@example.com>
2023-07-07 16:31:33 +0000

    add missing fields

--- types/webhook.go
+++ types/webhook.go
@@ -11,18 +11,20 @@ import (
 //
 // Webhook log
 type WebhookLogEntry struct {
-	ID         pgtype.UUID             `db:"id" json:"id" description:"The ID of the webhook log."`
-	TargetID   string                  `db:"target_id" json:"target_id" description:"The target ID."`
-	TargetType string                  `db:"target_type" json:"target_type" description:"The target type (bot/team etc.)."`
-	UserID     string                  `db:"user_id" json:"-"`
-	User       *dovetypes.PlatformUser `json:"user" description:"User ID the webhook is intended for" ci:"internal"` // Must be parsed internally
-	URL        string                  `db:"url" json:"url" description:"The URL of the webhook."`
-	Data       map[string]any          `db:"data" json:"data" description:"The data of the webhook."`
-	Sign       string                  `db:"sign" json:"sign" description:"The auth secret of the webhook."`
-	CreatedAt  time.Time               `db:"created_at" json:"created_at" description:"The time when the webhook was created."`
-	State      string                  `db:"state" json:"state" description:"The state of the webhook."`
-	Tries      int                     `db:"tries" json:"tries" description:"The number of send tries attempted on this webhook."`
-	LastTry    time.Time               `db:"last_try" json:"last_try" description:"The time of the last send try."`
+	ID          pgtype.UUID             `db:"id" json:"id" description:"The ID of the webhook log."`
+	TargetID    string                  `db:"target_id" json:"target_id" description:"The target ID."`
+	TargetType  string                  `db:"target_type" json:"target_type" description:"The target type (bot/team etc.)."`
+	UserID      string                  `db:"user_id" json:"-"`
+	User        *dovetypes.PlatformUser `json:"user" description:"User ID the webhook is intended for" ci:"internal"` // Must be parsed internally
+	URL         string                  `db:"url" json:"url" description:"The URL of the webhook."`
+	Data        map[string]any          `db:"data" json:"data" description:"The data of the webhook."`
+	Sign        string                  `db:"sign" json:"sign" description:"The auth secret of the webhook."`
+	CreatedAt   time.Time               `db:"created_at" json:"created_at" description:"The time when the webhook was created."`
+	State       string                  `db:"state" json:"state" description:"The state of the webhook."`
+	Tries       int                     `db:"tries" json:"tries" description:"The number of send tries attempted on this webhook."`
+	LastTry     time.Time               `db:"last_try" json:"last_try" description:"The time of the last send try."`
+	BadIntent   bool                    `db:"bad_intent" json:"bad_intent" description:"Whether the webhook was sent with bad intent."`
+	UseInsecure bool                    `db:"use_insecure" json:"use_insecure" description:"Whether the webhook should be sent with an insecure connection."`
 }
 
 type PatchBotWebhook struct {
