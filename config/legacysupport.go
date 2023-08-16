157ba21c2a8c14f9d086c9c8ca1a4165ed52d2e9
Your Name <you@example.com>
2023-08-16 12:43:44 +0000

    remove threaded, not working webhook

--- config/legacysupport.go
+++ config/legacysupport.go
@@ -8,7 +8,6 @@ var legacyWebhooks = [...]string{
 	"434556304661544960",  // Waifu, builderb
 	"827234880122650654",  // Succubus, marshall
 	"845214061511180298",  // Wyvern, Ben/Connor
-	"1068627569760550994", // Threaded, thatbadname
 	"587152333519978559",  // Primo, compiles (Dan), ashmw (AshMW)
 	"892836355074306058",  // GamingBuddy, muckmuck96
 	"924540290461736971",  // Watcher, frostlord_
