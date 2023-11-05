1440ce698bb3e05f21443f84b0f3d0fc25cdf11e
Your Name <you@example.com>
2023-11-05 11:10:46 +0000

    fix

--- types/ticket.go
+++ types/ticket.go
@@ -35,9 +35,11 @@ type Message struct {
 }
 
 type Attachment struct {
-	ID       string   `json:"id"`        // ID of the attachment within the ticket
-	URL      string   `json:"url"`       // URL of the attachment
-	ProxyURL string   `json:"proxy_url"` // URL (cached) of the attachment
-	Name     string   `json:"name"`      // Name of the attachment
-	Errors   []string `json:"errors"`    // Non-fatal errors that occurred while uploading the attachment
+	ID          string   `json:"id"`           // ID of the attachment within the ticket
+	URL         string   `json:"url"`          // URL of the attachment
+	ProxyURL    string   `json:"proxy_url"`    // URL (cached) of the attachment
+	Name        string   `json:"name"`         // Name of the attachment
+	ContentType string   `json:"content_type"` // Content type of the attachment
+	Size        int      `json:"size"`         // Size of the attachment in bytes
+	Errors      []string `json:"errors"`       // Non-fatal errors that occurred while uploading the attachment
 }
