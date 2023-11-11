073af8f7643989ed3cc5ed89211bb95a0941847e
Your Name <you@example.com>
2023-11-11 06:51:39 +0000

    published flag

--- types/changelogs.go
+++ types/changelogs.go
@@ -13,6 +13,7 @@ type ChangelogEntry struct {
 	Version          string      `db:"version" json:"version" validate:"required" description:"The version for the changelog entry. (4.3.0 etc.)"`
 	ExtraDescription string      `db:"extra_description" json:"extra_description" description:"The extra description for the version, if applicable"`
 	GithubHTML       pgtype.Text `db:"github_html" json:"github_html" description:"The Github-backed HTML for the changelog entry."`
+	Published        bool        `db:"published" json:"published" description:"Whether or not this is a published changelog"`
 	Prerelease       bool        `db:"prerelease" json:"prerelease" description:"Whether or not this is a prerelease."`
 	Added            []string    `db:"added" json:"added" validate:"required" description:"The added features for the version."`
 	Updated          []string    `db:"updated" json:"updated" validate:"required" description:"The changed features for the version."`
