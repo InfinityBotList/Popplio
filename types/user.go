1c79125aad1107f02580816b19408ea7d578d677
Your Name <you@example.com>
2023-12-22 06:02:30 +0000

    remove staff onboarding data from users table

--- types/user.go
+++ types/user.go
@@ -13,28 +13,21 @@ const (
 
 // @ci table=users
 type User struct {
-	ITag                             pgtype.UUID             `db:"itag" json:"itag" description:"The user's internal ID. An artifact of database migrations."`
-	ID                               string                  `db:"user_id" json:"-"`
-	User                             *dovetypes.PlatformUser `db:"-" json:"user" ci:"internal"` // Must be handled internally
-	Experiments                      []string                `db:"experiments" json:"experiments" description:"The experiments the user is in"`
-	StaffOnboarded                   bool                    `db:"staff_onboarded" json:"staff_onboarded"`
-	StaffOnboardState                string                  `db:"staff_onboard_state" json:"staff_onboard_state"`
-	StaffOnboardLastStartTime        pgtype.Timestamptz      `db:"staff_onboard_last_start_time" json:"staff_onboard_last_start_time"`
-	StaffOnboardGuild                pgtype.Text             `db:"staff_onboard_guild" json:"staff_onboard_guild"`
-	StaffRPCLastVerify               pgtype.Timestamptz      `db:"staff_rpc_last_verify" json:"staff_rpc_last_verify"`
-	StaffOnboardSessionCode          pgtype.Text             `db:"staff_onboard_session_code" json:"-"`
-	StaffOnboardCurrentOnboardRespId pgtype.Text             `db:"staff_onboard_current_onboard_resp_id" json:"-"`
-	Certified                        bool                    `db:"certified" json:"certified"`
-	BotDeveloper                     bool                    `db:"developer" json:"bot_developer"`
-	BugHunters                       bool                    `db:"bug_hunters" json:"bug_hunters"`
-	CaptchaSponsorEnabled            bool                    `db:"captcha_sponsor_enabled" json:"captcha_sponsor_enabled"`
-	ExtraLinks                       []Link                  `db:"extra_links" json:"extra_links" description:"The users links that it wishes to advertise"`
-	About                            pgtype.Text             `db:"about" json:"about"`
-	VoteBanned                       bool                    `db:"vote_banned" json:"vote_banned"`
-	Banned                           bool                    `db:"banned" json:"banned"`
-	UserTeams                        []Team                  `db:"-" json:"user_teams" ci:"internal"` // Must be handled internally
-	UserBots                         []IndexBot              `db:"-" json:"user_bots" ci:"internal"`  // Must be handled internally
-	UserPacks                        []IndexBotPack          `db:"-" json:"user_packs" ci:"internal"` // Must be handled internally
+	ITag                  pgtype.UUID             `db:"itag" json:"itag" description:"The user's internal ID. An artifact of database migrations."`
+	ID                    string                  `db:"user_id" json:"-"`
+	User                  *dovetypes.PlatformUser `db:"-" json:"user" ci:"internal"` // Must be handled internally
+	Experiments           []string                `db:"experiments" json:"experiments" description:"The experiments the user is in"`
+	Certified             bool                    `db:"certified" json:"certified"`
+	BotDeveloper          bool                    `db:"developer" json:"bot_developer"`
+	BugHunters            bool                    `db:"bug_hunters" json:"bug_hunters"`
+	CaptchaSponsorEnabled bool                    `db:"captcha_sponsor_enabled" json:"captcha_sponsor_enabled"`
+	ExtraLinks            []Link                  `db:"extra_links" json:"extra_links" description:"The users links that it wishes to advertise"`
+	About                 pgtype.Text             `db:"about" json:"about"`
+	VoteBanned            bool                    `db:"vote_banned" json:"vote_banned"`
+	Banned                bool                    `db:"banned" json:"banned"`
+	UserTeams             []Team                  `db:"-" json:"user_teams" ci:"internal"` // Must be handled internally
+	UserBots              []IndexBot              `db:"-" json:"user_bots" ci:"internal"`  // Must be handled internally
+	UserPacks             []IndexBotPack          `db:"-" json:"user_packs" ci:"internal"` // Must be handled internally
 }
 
 type UserPerm struct {
